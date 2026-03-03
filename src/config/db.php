aa31c7a01c3a459b922e97593a0a6a35217d986c
Devin Aura Najmi <devinauranjmi123@gmail.com>
2026-03-03 20:55:20 +0700

    Database: Menambahkan konfigurasi koneksi

---
+++ src/config/db.php
@@ -0,0 +1,15 @@
+<?php
+// src/config/db.php
+session_start();
+$host = 'localhost';
+$dbname = 'db_rbpl_tkit_fr';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Koneksi database gagal: " . $e->getMessage());
+}
+?>
